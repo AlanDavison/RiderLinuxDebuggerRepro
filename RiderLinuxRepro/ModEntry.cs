1cfd8a2d21077394dcce6bb4ba66996d5a2374b2
Alan Davison <contact@decidedlyhuman.co.uk>
2024-06-01 19:10:49 +0100

    Initial commit.

---
+++ RiderLinuxRepro/ModEntry.cs
@@ -0,0 +1,18 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+namespace RiderLinuxRepro;
+
+public class ModEntry : Mod
+{
+    public override void Entry(IModHelper helper)
+    {
+        // Breakpoint above here for the earliest possible entry point for the mod.
+        helper.Events.GameLoop.GameLaunched += GameLaunched;
+    }
+
+    private void GameLaunched(object? sender, GameLaunchedEventArgs e)
+    {
+        // And right here for after the game has launched, before the first update tick.
+    }
+}
